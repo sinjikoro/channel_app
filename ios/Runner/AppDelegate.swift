4e5efcc1d057742d641f7162dbf39272af5ade5a
sinjikoro <shinnaga.dev@gmail.com>
2023-03-17 13:37:28 +0900

    362. Running iOS Code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,37 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+    override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    ) -> Bool {
+
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let batteryChannel = FlutterMethodChannel(name: "course.flutter.dev/buttery",
+                                                binaryMessenger: controller.binaryMessenger)
+        batteryChannel.setMethodCallHandler({
+          [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          // This method is invoked on the UI thread.
+          guard call.method == "getBatteryLevel" else {
+            result(FlutterMethodNotImplemented)
+            return
+          }
+          self?.receiveBatteryLevel(result: result)
+        })
+
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    private func receiveBatteryLevel(result: FlutterResult) {
+        let device = UIDevice.current
+        device.isBatteryMonitoringEnabled = true
+        if device.batteryState == UIDevice.BatteryState.unknown {
+        result(FlutterError(code: "UNAVAILABLE",
+                            message: "Battery level not available.",
+                            details: nil))
+        } else {
+            result(Int(device.batteryLevel * 100))
+        }
+    }
 }
